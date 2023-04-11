f1712a373635562b6be60042c9e780494d95d963
haochen2019 <dhaochen@163.com>
2023-04-11 20:08:05 +0800

    lab01 finish

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,15 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+int ll_has_cycle(node* head) {
+  /* your code here */
+  node *slow = head, *fast = head;
+  while (fast && fast->next) {
+    fast = fast->next->next;
+    slow = slow->next;
+    if (fast == slow) {
+      return 1;
+    }
+  }
+  return 0;
 }
